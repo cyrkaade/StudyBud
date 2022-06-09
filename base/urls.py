98471a77a029ac52f73e866d13ffb4c7627ba781
Arslan <76843389+cyrkaade@users.noreply.github.com>
2022-06-09 21:58:43 +0600

    Add files via upload

---
+++ base/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('', views.home, name = "home"),
+    path('room/<str:pk>/', views.room, name = "room"),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('register/', views.registerPage, name="register"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+    path('update-user/', views.updateUser, name="update-user"),
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
